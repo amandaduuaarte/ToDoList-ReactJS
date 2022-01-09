742888e44ee9dbc93f983c3191fd03745482d0bc
Amanda <amandaduuaarte@gmail.com>
2022-01-09 14:52:36 -0300

    New task

--- src/components/ModalNewTaks/index.tsx
+++ src/components/ModalNewTaks/index.tsx
@@ -1,14 +1,31 @@
 import { Container } from "./style"
 import Modal from 'react-modal'
 import CloseImg from '../../assets/imgs/Closer.svg'
+import { useState } from "react"
 
 interface modalProps {
   modalIsOpen:boolean,
   onRequestClose: ()=> void 
 }
 
+interface taskProps {
+  title:string,
+  description:string
+}
 export function ModalNewTaks ({modalIsOpen,onRequestClose}  :modalProps){
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
  
+  const [task, setTask] = useState<taskProps[]>([]);
+
+  const handleNewTask = ()=>{
+    const newTask = {
+      title:title,
+      description:description
+    }
+    setTask(oldState =>[...oldState, newTask]);
+    setTitle(''); 
+  }
   return(
     <>
       <Modal 
@@ -22,8 +39,14 @@ export function ModalNewTaks ({modalIsOpen,onRequestClose}  :modalProps){
          <img src={CloseImg} alt="closeIcon"/>
         </button>
         <h2>New Task</h2>
-        <input type="input" placeholder="Title"/>
-        <input type="text"  placeholder="Description" />
+        <input 
+        onChange={(e) => setTitle(e.target.value)}
+        type="input" 
+        placeholder="Title"/>
+        <input type="text"  
+        placeholder="Description" 
+        onChange={(e) => setDescription(e.target.value)}
+        />
         <button type="submit">Save</button>
       </Container>
       </Modal>
